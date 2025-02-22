f00123af39365b35d2027f720910042f69063272
Tanmoy paul <84624966+tanmoypaul1005@users.noreply.github.com>
2025-02-22 17:52:26 +0600

    refactor: update layout title and replace logo image in MainNav

--- components/main-nav.jsx
+++ components/main-nav.jsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 import Link from "next/link";
 import { cn } from "@/lib/utils";
 import { MobileNav } from "@/components/mobile-nav";
-import lwsLogo from "@/assets/lws_logo.svg";
+import lwsLogo from "@/assets/logo.png";
 import Image from "next/image";
 import { X } from "lucide-react";
 import { Button, buttonVariants } from "./ui/button";
@@ -26,7 +26,7 @@ export function MainNav({ items, children }) {
 		<>
 			<div className="flex gap-6 lg:gap-10">
 				<Link href="/">
-					<Image className="max-w-[100px]" src={lwsLogo} alt="Logo" />
+					<Image width={50} height={30} className="max-w-[100px]" src={lwsLogo} alt="Logo" />
 				</Link>
 				{items?.length ? (
 					<nav className="hidden gap-6 lg:flex">
