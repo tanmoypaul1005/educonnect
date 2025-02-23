c41788c40b774fa7c45d3ff023f061d8adf9a8b0
Tanmoy paul <84624966+tanmoypaul1005@users.noreply.github.com>
2025-02-24 00:11:44 +0600

    refactor: replace testimonial entries with TestimonialCard components for improved readability and maintainability

--- app/(main)/(components)/TestimonialSlider.jsx
+++ app/(main)/(components)/TestimonialSlider.jsx
@@ -106,128 +106,46 @@ const TestimonialSlider = () => {
 
                 <ul className="hidden space-y-8 lg:block">
                     <li className="text-sm leading-6">
-                        <div className="relative group">
-                            <div className="absolute transition rounded-lg opacity-25 -inset-1 bg-gradient-to-r from-purple-600 to-pink-600 blur duration-400 group-hover:opacity-100 group-hover:duration-200"></div>
-                            <a
-                                href="https://twitter.com/satyanadella"
-                                className="cursor-pointer"
-                            >
-                                <div className="relative p-6 space-y-6 leading-none rounded-lg bg-slate-800 ring-1 ring-gray-900/5">
-                                    <div className="flex items-center space-x-4">
-                                        <img
-                                            src="https://pbs.twimg.com/profile_images/1221837516816306177/_Ld4un5A_400x400.jpg"
-                                            className="w-12 h-12 bg-center bg-cover border rounded-full"
-                                            alt="Satya Nadella"
-                                        />
-                                        <div>
-                                            <h3 className="text-lg font-semibold text-white">
-                                                Satya Nadella
-                                            </h3>
-                                            <p className="text-gray-500 text-md">
-                                                CEO of Microsoft
-                                            </p>
-                                        </div>
-                                    </div>
-                                    <p className="leading-normal text-gray-300 text-md">
-                                        Tortor dignissim convallis aenean et tortor at. At
+                        <TestimonialCard 
+                            name="Satya Nadella"
+                            title="CEO of Microsoft"
+                            image="https://pbs.twimg.com/profile_images/1221837516816306177/_Ld4un5A_400x400.jpg"
+                            testimonial="Tortor dignissim convallis aenean et tortor at. At
                                         ultrices mi tempus imperdiet nulla malesuada. Id cursus
                                         metus aliquam eleifend mi. Quis ipsum suspendisse
-                                        ultrices gravida dictum fusce ut.
-                                    </p>
-                                </div>
-                            </a>
-                        </div>
+                                        ultrices gravida dictum fusce ut."
+                        
+                        />
                     </li>
                     <li className="text-sm leading-6">
-                        <div className="relative group">
-                            <div className="absolute transition rounded-lg opacity-25 -inset-1 bg-gradient-to-r from-purple-600 to-pink-600 blur duration-400 group-hover:opacity-100 group-hover:duration-200"></div>
-                            <a
-                                href="https://twitter.com/dan_schulman"
-                                className="cursor-pointer"
-                            >
-                                <div className="relative p-6 space-y-6 leading-none rounded-lg bg-slate-800 ring-1 ring-gray-900/5">
-                                    <div className="flex items-center space-x-4">
-                                        <img
-                                            src="https://pbs.twimg.com/profile_images/516916920482672641/3jCeLgFb_400x400.jpeg"
-                                            className="w-12 h-12 bg-center bg-cover border rounded-full"
-                                            alt="Dan Schulman"
-                                        />
-                                        <div>
-                                            <h3 className="text-lg font-semibold text-white">
-                                                Dan Schulman
-                                            </h3>
-                                            <p className="text-gray-500 text-md">CEO of PayPal</p>
-                                        </div>
-                                    </div>
-                                    <p className="leading-normal text-gray-300 text-md">
-                                        Quam pellentesque nec nam aliquam sem et tortor
-                                        consequat id. Enim sit amet venenatis urna cursus.
-                                    </p>
-                                </div>
-                            </a>
-                        </div>
+                        <TestimonialCard 
+                            name="Dan Schulman"
+                            title="CEO of PayPal"
+                            image="https://pbs.twimg.com/profile_images/516916920482672641/3jCeLgFb_400x400.jpeg"
+                            testimonial="Quam pellentesque nec nam aliquam sem et tortor
+                                        consequat id. Enim sit amet venenatis urna cursus."
+                        />
                     </li>
                     <li className="text-sm leading-6">
-                        <div className="relative group">
-                            <div className="absolute transition rounded-lg opacity-25 -inset-1 bg-gradient-to-r from-purple-600 to-pink-600 blur duration-400 group-hover:opacity-100 group-hover:duration-200"></div>
-                            <a
-                                href="https://twitter.com/satyanadella"
-                                className="cursor-pointer"
-                            >
-                                <div className="relative p-6 space-y-6 leading-none rounded-lg bg-slate-800 ring-1 ring-gray-900/5">
-                                    <div className="flex items-center space-x-4">
-                                        <img
-                                            src="https://pbs.twimg.com/profile_images/1221837516816306177/_Ld4un5A_400x400.jpg"
-                                            className="w-12 h-12 bg-center bg-cover border rounded-full"
-                                            alt="Satya Nadella"
-                                        />
-                                        <div>
-                                            <h3 className="text-lg font-semibold text-white">
-                                                Satya Nadella
-                                            </h3>
-                                            <p className="text-gray-500 text-md">
-                                                CEO of Microsoft
-                                            </p>
-                                        </div>
-                                    </div>
-                                    <p className="leading-normal text-gray-300 text-md">
-                                        Tortor dignissim convallis aenean et tortor at. At
+                        <TestimonialCard 
+                            name="Satya Nadella"
+                            title="CEO of Microsoft"
+                            image="https://pbs.twimg.com/profile_images/1221837516816306177/_Ld4un5A_400x400.jpg"
+                            testimonial="Tortor dignissim convallis aenean et tortor at. At
                                         ultrices mi tempus imperdiet nulla malesuada. Id cursus
                                         metus aliquam eleifend mi. Quis ipsum suspendisse
-                                        ultrices gravida dictum fusce ut.
-                                    </p>
-                                </div>
-                            </a>
-                        </div>
+                                        ultrices gravida dictum fusce ut."
+                        />
+
                     </li>
                     <li className="text-sm leading-6">
-                        <div className="relative group">
-                            <div className="absolute transition rounded-lg opacity-25 -inset-1 bg-gradient-to-r from-purple-600 to-pink-600 blur duration-400 group-hover:opacity-100 group-hover:duration-200"></div>
-                            <a
-                                href="https://twitter.com/dan_schulman"
-                                className="cursor-pointer"
-                            >
-                                <div className="relative p-6 space-y-6 leading-none rounded-lg bg-slate-800 ring-1 ring-gray-900/5">
-                                    <div className="flex items-center space-x-4">
-                                        <img
-                                            src="https://pbs.twimg.com/profile_images/516916920482672641/3jCeLgFb_400x400.jpeg"
-                                            className="w-12 h-12 bg-center bg-cover border rounded-full"
-                                            alt="Dan Schulman"
-                                        />
-                                        <div>
-                                            <h3 className="text-lg font-semibold text-white">
-                                                Dan Schulman
-                                            </h3>
-                                            <p className="text-gray-500 text-md">CEO of PayPal</p>
-                                        </div>
-                                    </div>
-                                    <p className="leading-normal text-gray-300 text-md">
-                                        Quam pellentesque nec nam aliquam sem et tortor
-                                        consequat id. Enim sit amet venenatis urna cursus.
-                                    </p>
-                                </div>
-                            </a>
-                        </div>
+                        <TestimonialCard 
+                            name="Dan Schulman"
+                            title="CEO of PayPal"
+                            image="https://pbs.twimg.com/profile_images/516916920482672641/3jCeLgFb_400x400.jpeg"
+                            testimonial="Quam pellentesque nec nam aliquam sem et tortor
+                                        consequat id. Enim sit amet venenatis urna cursus."
+                        />
                     </li>
                 </ul>
             </div>
