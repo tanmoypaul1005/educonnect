803f508c861e8594665796f8ec590aad4e5befeb
Tanmoy paul <84624966+tanmoypaul1005@users.noreply.github.com>
2025-02-26 21:42:05 +0600

    refactor: add "Enrolled Courses" link to CustomLayout for improved navigation

--- app/(main)/page.jsx
+++ app/(main)/page.jsx
@@ -42,7 +42,7 @@ const HomePage = () => {
               Explore Now
             </Link>
             <Link
-              href=""
+              href="/instructor"
               className={cn(buttonVariants({ variant: "outline", size: "lg" }))}
             >
               Become An Instructor
